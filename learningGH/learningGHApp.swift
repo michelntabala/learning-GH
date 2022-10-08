a06dfd69d72067f166827b737b6690964103c1fe
michel ntabala <michelntabala@gmail.com>
2022-10-08 13:26:23 +0300

    Initial Commit

---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by Mac on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
